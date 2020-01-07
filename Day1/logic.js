0303a3056c398c66d06b4bfe4162c04abf13afbd
Hunter Haak <ministash14@gmail.com>
2020-01-06 22:09:59 -0700

    reworking things to be better

--- Day1/logic.js
+++ Day1/logic.js
@@ -1,53 +1,48 @@
-// Variables Defined
-const isClicked = false;
-let whosTurnIsItCurrently = false;
-let colorArr = ["blue", "red"];
-let currentColor = '';
 const boxObj = [
     {
         id: "0",
         hasClicked: false,
-        playerChoice: ""
+        playerChoice: "white"
     },
     {
         id: "1",
         hasClicked: false,
-        playerChoice: ""
+        playerChoice: "white"
     },
     {
         id: "2",
         hasClicked: false,
-        playerChoice: ""
+        playerChoice: "white"
     },
     {
         id: "3",
         hasClicked: false,
-        playerChoice: ""
+        playerChoice: "white"
     },
     {
         id: "4",
         hasClicked: false,
-        playerChoice: ""
+        playerChoice: "white"
     },
     {
         id: "5",
         hasClicked: false,
-        playerChoice: ""
+        playerChoice: "white"
     },
     {
         id: "6",
         hasClicked: false,
-        playerChoice: ""
+        playerChoice: "white"
     },
     {
         id: "7",
         hasClicked: false,
-        playerChoice: ""
+        playerChoice: "white"
     },
     {
         id: "8",
         hasClicked: false,
-        playerChoice: ""
+        playerChoice: "white"
     },
 
 ];
@@ -56,66 +51,104 @@ const boxObj = [
 
 
 
-let startGame = () =>{
-        boxObj.forEach(function(item) {
-            $('.checkerboard-holder').append(
-                $("<div/>")
-                .addClass('box')
-                .attr('id', item.id)
-            );
-        });
+let startGame = (turn) =>{
 
-    clickFunction();
-    colorChanger();
+    // console.log("startGame is working");
+    whosTurnIsItGameStart();
+    buildDivFunc();
+    clickFunction(turn);
 
 }
 
-let clickFunction = () => {
+let buildDivFunc = () =>{
     
-        $(".box").on("click", function(e){
-            e.preventDefault();
-
-            let currentBox = event.target.id;
-            let locateObjNum = currentBox.toString();
-
-            for(let i = 0; i < boxObj.length; i++){
-                if(locateObjNum === boxObj[i].id && boxObj[i].hasClicked === false){
-                    boxObj[locateObjNum].hasClicked = true;
-                    colorChanger();
-                }else{
-                    console.log("not working");
-                }
-            }
-    });
+    boxObj.forEach(function(item) {
+        $('.checkerboard-holder').append(
+            $("<div/>")
+            .addClass('box')
+            .attr('id', item.id)
+        );
+    }); 
 }
 
-let colorChanger = () =>{
-    for(let i = 0; i < boxObj.length; i++){
-        if(boxObj[i].hasClicked === false){
 
-        }else{
+let clickFunction = (currentTurn) =>{
+    console.log(currentTurn);
+    $(".box").on("click", function(e){
+        e.preventDefault();
+        // console.log("click is working");
+         let newId = event.target.id;
+         gameFunction(newId, currentTurn);
+    }); 
+}
+
+let gameFunction = (id, currentPlayer) =>{
+
+        // console.log("update is happening");
+        updateClickedDivs(id);
+        updatePlayerChoice(id, currentPlayer);
+        // updateCurrentTurn(currentPlayer);
+    
+}
+
+
+let updateClickedDivs = (id) =>{
+    for (let index = 0; index < boxObj.length; index++) {
+        if(boxObj[index].id === id && boxObj[index].hasClicked === false){
+            boxObj[index].hasClicked = true;
             console.log(boxObj);
+            // console.log("we found the corret object!");
+        }else{
+            // console.log('this is the wrong item in your objects');
+        }
+    }  
+}
 
-            $('#' + boxObj[i].id).css('background-color', currentColor);
-            
+
+let updatePlayerChoice = (id, currentPlayerChoice) =>{
+    console.log(currentPlayerChoice);
+    if(currentPlayerChoice === 0){
+        currentPlayerChoice = "red";
+    }else{
+        currentPlayerChoice = "blue";
+        console.log("blue");
+    }
+
+    for (let index = 0; index < boxObj.length; index++) {
+        if(boxObj[index].id === id){
+            boxObj[index].playerChoice = currentPlayerChoice;
         }
     }
-}
+} 
+
+// let updateCurrentTurn = (currentPlayer) =>{
+//     if(currentPlayer === currentPlayer){
+
+//     }else{
+//         if (currentPlayer === 0){
+//             return currentPlayer = 1;
+
+//         }else{
+//             return currentPlayer = 0;
+
+//         }
+
+//     }
+// }
 
 
-let whosTurnIsIt= () =>{
-   let randomDecider = Math.floor(Math.random() * 2);
-   
-   if(randomDecider === 0){
-       whosTurnIsItCurrently = false;
-        currentColor = colorArr[0]
-
-   }else{
-    whosTurnIsItCurrently = true;
-    currentColor = colorArr[1]
-   }
-}
 
 
-startGame();
-whosTurnIsIt();
+
+
+
+
+
+
+
+let whosTurnIsItGameStart = () => {
+    return  Math.floor(Math.random() * 2);
+};
+
+
+startGame(whosTurnIsItGameStart());
