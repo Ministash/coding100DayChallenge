60e2f457f214861bfd4dce5e01bcbdd02dcda36c
Hunter Haak <ministash14@gmail.com>
2020-01-10 13:51:03 -0700

    working on the winning logic rn

--- Day1/logic.js
+++ Day1/logic.js
@@ -3,47 +3,47 @@ const boxObj = [
     {
         id: "0",
         hasClicked: false,
-        playerChoice: "white"
+        playerChoice: ""
     },
     {
         id: "1",
         hasClicked: false,
-        playerChoice: "white"
+        playerChoice: ""
     },
     {
         id: "2",
         hasClicked: false,
-        playerChoice: "white"
+        playerChoice: ""
     },
     {
         id: "3",
         hasClicked: false,
-        playerChoice: "white"
+        playerChoice: ""
     },
     {
         id: "4",
         hasClicked: false,
-        playerChoice: "white"
+        playerChoice: ""
     },
     {
         id: "5",
         hasClicked: false,
-        playerChoice: "white"
+        playerChoice: ""
     },
     {
         id: "6",
         hasClicked: false,
-        playerChoice: "white"
+        playerChoice: ""
     },
     {
         id: "7",
         hasClicked: false,
-        playerChoice: "white"
+        playerChoice: ""
     },
     {
         id: "8",
         hasClicked: false,
-        playerChoice: "white"
+        playerChoice: ""
     },
 
 ];
@@ -68,6 +68,7 @@ let buildDivFunc = () =>{
     }); 
 
     clickFunction();
+    
 
 }
 
@@ -84,13 +85,14 @@ let clickFunction = () =>{
 
 let gameFunction = (id, currentPlayer) =>{
     if(boxObj[id].hasClicked === true){
-        console.log('nothing will be done my child');
+        // console.log('nothing will be done my child');
     }else{
         $('.checkerboard-holder').html("");
         updateClickedDivs(id);
         updatePlayerChoice(id, currentPlayer);
         updateCurrentTurn(id);
         buildDivFunc();
+        hasSomeoneWon();
 
     }
     
@@ -129,30 +131,51 @@ let updateCurrentTurn = (id) =>{
             if (currentPlayer === 0){
                 currentPlayer = 1;
                 // console.log("new player is red");
-                console.log(boxObj);
+                // console.log(boxObj);
     
             }else{
                  currentPlayer = 0;
                 //  console.log("new player is blue");
-                 console.log(boxObj);
+                //  console.log(boxObj);
     
             }
 
     }
 
 
+let hasSomeoneWon = () =>{
+    // let redWinnings = [];
+    // let blueWinnings = [];
+    
+
+    let blueWinnings = boxObj.filter(choice => {
+        return choice.playerChoice === "blue";
+    });
 
 
 
+        
+        
+    //     else if(boxObj[index].playerChoice === "blue"){
+    //         for (let j = 0; j <  blueWinnings.length; j++) {
+    //             // if( blueWinnings[j] != boxObj[index].id)
+    //             // blueWinnings.push(boxObj[index].id);
+    //             // scoreChecker( blueWinnings);
+    //             console.log(boxObj[index].id);
+    //         }
+
+    //     }else{
+    //         console.log("nothing is being pushed");
+    //     }
+    // }
+} 
+
+// let scoreChecker = (scoreArr) =>{
+//     console.log(scoreArr);
+// }
 
 
 
-
-
-
-// let whosTurnIsItGameStart = () => {
-//     return  Math.floor(Math.random() * 2);
-// };
 
 
 startGame();
